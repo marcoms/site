8c28482257bdfc886241fdbc4bbb9c4aa9c70efc
Marco Scannadinari <m@scannadinari.co.uk>
2016-09-20 17:07:43 +0100

    bigger links, better description

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-"use strict";
+`use strict`;
 
-const gulp = require("gulp");
+const gulp = require(`gulp`);
 
-const metalsmith = require("metalsmith");
-const metalLayouts = require("metalsmith-layouts");
-const metalPostcss = require("metalsmith-postcss");
-const metalHtmlMin = require("metalsmith-html-minifier");
-const metalCleanCss = require("metalsmith-clean-css");
+const metalsmith = require(`metalsmith`);
+const metalLayouts = require(`metalsmith-layouts`);
+const metalPostcss = require(`metalsmith-postcss`);
+const metalHtmlMin = require(`metalsmith-html-minifier`);
+const metalCleanCss = require(`metalsmith-clean-css`);
 
-const cssImport = require("postcss-import");
-const cssSimpleVars = require("postcss-simple-vars");
-const cssSize = require("postcss-size");
-const cssEasings = require("postcss-easings");
-const cssPrefixer = require("autoprefixer");
+const cssImport = require(`postcss-import`);
+const cssSimpleVars = require(`postcss-simple-vars`);
+const cssSize = require(`postcss-size`);
+const cssEasings = require(`postcss-easings`);
+const cssPrefixer = require(`autoprefixer`);
 
-const METALSMITH_DIR = __dirname + "/metalsmith";
+const METALSMITH_DIR = __dirname + `/metalsmith`;
 
-gulp.task("default", ["build"]);
+gulp.task(`default`, [`build`]);
 
-gulp.task("build", () => {
+gulp.task(`build`, () => {
 	metalsmith(METALSMITH_DIR)
-	.ignore("**/img/**/.git")
+	.ignore(`**/img/**/.git`)
 	.use(metalLayouts({
-		engine: "handlebars",
-		default: "base.hbs",
-		directory: "layouts",
-		partials: "partials",
-		pattern: ["*.html"],
+		engine: `handlebars`,
+		default: `base.hbs`,
+		directory: `layouts`,
+		partials: `partials`,
+		pattern: [`*.html`],
 	}))
 
 	.use(metalPostcss([
@@ -37,7 +37,7 @@ gulp.task("build", () => {
 		cssSimpleVars(),
 		cssSize(),
 		cssEasings(),
-		cssPrefixer({browsers: ["last 3 versions"]}),
+		cssPrefixer({browsers: [`last 3 versions`]}),
 	]))
 
 	.use(metalHtmlMin({
@@ -47,7 +47,7 @@ gulp.task("build", () => {
 	.use(metalCleanCss())
 
 	.clean(false)
-	.destination("..")
+	.destination(`..`)
 	.build((error, files) => {
 		if (error) {
 			console.log(`error: ${error}`);
@@ -59,6 +59,6 @@ gulp.task("build", () => {
 	});
 });
 
-gulp.task("watch", ["build"], () => {
-	gulp.watch("metalsmith/**/*", ["build"]);
+gulp.task(`watch`, [`build`], () => {
+	gulp.watch(`metalsmith/**/*`, [`build`]);
 });
